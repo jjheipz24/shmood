7d6f866240fa1b91a40f227e9e6a556c3fa17324
Jin Jin Heipler (RIT Student) <jxh9922@ad.rit.edu>
2019-11-20 15:21:54 -0500

    added files and helper function

---
+++ client/helper/helper.js
@@ -0,0 +1,47 @@
+//handles all error messages
+const showError = (message) => {
+    $(".error").text(message);
+    $(".error").fadeIn(400);
+  }
+  
+  //handles requests
+  const sendAjax = (action, data) => {
+    $.ajax({
+      cache: false,
+      type: "POST",
+      url: action,
+      data: data,
+      dataType: "json",
+      success: (result, status, xhr) => {
+        $(".error").fadeOut(400);
+  
+        window.location = result.redirect;
+      },
+      error: (xhr, status, error) => {
+        const messageObj = JSON.parse(xhr.responseText);
+  
+        showError(messageObj.error);
+      }
+    });
+  }
+  
+  const fileUpload = (action, data) => {
+    $.ajax({
+      cache: false,
+      type: "POST",
+      url: action,
+      data: data,
+      processData: false,
+      contentType: false,
+      success: (result, status, xhr) => {
+        $(".error").fadeOut(400);
+  
+        window.location = result.redirect;
+      },
+      error: (xhr, status, error) => {
+        const messageObj = JSON.parse(xhr.responseText);
+  
+        showError(messageObj.error);
+      }
+    });
+  }
