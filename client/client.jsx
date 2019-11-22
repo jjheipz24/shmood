06ced16b48fb0aae506221b58b87b0d27c6a72a7
Jin Jin Heipler (RIT Student) <jxh9922@ad.rit.edu>
2019-11-22 13:29:47 -0500

    created handlesignup function

---
+++ client/client.jsx
@@ -0,0 +1,40 @@
+$("#signupForm").on("submit", (e) => {
+    e.preventDefault();
+
+    $(".error").fadeOut(400);
+
+    if ($("#user").val() == '' || $("#pass").val() == '' || $("#pass2").val() == '') {
+      showError("All fields are required");
+      return false;
+    }
+
+    if ($("#pass").val() !== $("#pass2").val()) {
+      showError("Passwords do not match");
+      return false;
+    }
+
+    sendAjax($("#signupForm").attr("action"), $("#signupForm").serialize());
+
+    return false;
+  });
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+
+    $(".error").fadeOut(400);
+
+    if ($("#user").val() == '' || $("#pass").val() == '' || $("#pass2").val() == '') {
+      showError("All fields are required");
+      return false;
+    }
+
+    if ($("#pass").val() !== $("#pass2").val()) {
+      showError("Passwords do not match");
+      return false;
+    }
+    
+    sendAjax($("#signupForm").attr("action"), $("#signupForm").serialize());
+
+    return false;
+    
+  };
