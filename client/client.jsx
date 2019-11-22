1f90b3c650a7dd430df0061152237cc98dc8a213
Jin Jin Heipler (RIT Student) <jxh9922@ad.rit.edu>
2019-11-22 13:41:08 -0500

    created handlelogin and changepassword

--- client/client.jsx
+++ client/client.jsx
@@ -1,23 +1,3 @@
-$("#signupForm").on("submit", (e) => {
-    e.preventDefault();
-
-    $(".error").fadeOut(400);
-
-    if ($("#user").val() == '' || $("#pass").val() == '' || $("#pass2").val() == '') {
-      showError("All fields are required");
-      return false;
-    }
-
-    if ($("#pass").val() !== $("#pass2").val()) {
-      showError("Passwords do not match");
-      return false;
-    }
-
-    sendAjax($("#signupForm").attr("action"), $("#signupForm").serialize());
-
-    return false;
-  });
-
   const handleSignup = (e) => {
     e.preventDefault();
 
@@ -38,3 +18,43 @@ $("#signupForm").on("submit", (e) => {
     return false;
     
   };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    $(".error").fadeOut(400);
+
+    if ($("#user").val() == '') {
+      showError("Username is required");
+      return false;
+    }
+
+    if ($("#pass").val() == '') {
+      showError("Password is required");
+      return false;
+    }
+
+    sendAjax($("#loginForm").attr("action"), $("#loginForm").serialize());
+
+    return false;
+  }
+
+  const handleChangePassword = (e) => {
+    e.preventDefault();
+
+    $(".error").fadeOut(400);
+
+    if ($("#currentPass").val() == '' || $("#newPass").val() == '' || $("#pass2").val() == '') {
+      showError("All fields are required");
+      return false;
+    }
+
+    if ($("#newPass").val() !== $("#pass2").val()) {
+      showError("Passwords do not match")
+      return false;
+    }
+
+    sendAjax($("#changePasswordForm").attr("action"), $("#changePasswordForm").serialize());
+
+    return false;
+  }
