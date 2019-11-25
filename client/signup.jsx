56a77e0ca923996e223a1701db753aa89864945c
Jin Jin Heipler (RIT Student) <jxh9922@ad.rit.edu>
2019-11-25 13:49:09 -0500

    signup form might render??

--- client/signup.jsx
+++ client/signup.jsx
@@ -18,3 +18,46 @@ const handleSignup = (e) => {
     return false;
     
   };
+
+  const SignupForm = (props) => {
+      return (
+        <form id="signupForm" name="signupForm" onSubmit={handleSignup} action="/signup" method="POST" class="mainForm">
+        <div class="greeting">
+          <img class="icon" src="./assets/img/largeLogo.png" alt="logo for Shmood" />>
+          <h1>WELCOME</h1>
+        </div>
+
+        <div class="fields">
+          <div class="form-group col-xs-4">
+            <input class="field" id="user" type="text" name="username" placeholder="Username" />
+          </div>
+          <div class="form-group col-xs-4">
+            <input class="field" id="pass" type="password" name="pass" placeholder="Password" />
+          </div>
+          <div class="form-group col-xs-4">
+            <input class="field" id="pass2" type="password" name="pass2" placeholder="Retype password" />
+          </div>
+        </div>
+        <input id="signupCsrf" type="hidden" name="_csrf" value={props.csrf} />
+        <button class="btn btn-secondary rounded-pill" id="signupButton" type="submit">Sign Up</button>
+
+        <div class="alert alert-danger error" role="alert">
+          Passwords do not match
+        </div>
+
+        <p>Already have an account? <a href="./login">Login</a></p>
+      </form>
+      );
+  };
+
+  const setup = function(csrf){
+    ReactDOM.render(
+      <SignupForm csrf={csrf} />, document.querySelector('#signup')
+    );
+  };
+
+  const getToken = () => {
+    sendGenericAjax('GET', '/getToken', null, (result) => {
+        setup(result.csrfToken);
+    });
+};
