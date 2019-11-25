c785f6a84bd4da0a3c96cddf3db84cced00bdc67
Jin Jin Heipler (RIT Student) <jxh9922@ad.rit.edu>
2019-11-25 14:59:49 -0500

    uncommented

--- client/login.jsx
+++ client/login.jsx
@@ -1,58 +1,58 @@
-//   const handleLogin = (e) => {
-//     e.preventDefault();
+  const handleLogin = (e) => {
+    e.preventDefault();
 
-//     $(".error").fadeOut(400);
+    $(".error").fadeOut(400);
 
-//     if ($("#user").val() == '') {
-//       showError("Username is required");
-//       return false;
-//     }
+    if ($("#user").val() == '') {
+      showError("Username is required");
+      return false;
+    }
 
-//     if ($("#pass").val() == '') {
-//       showError("Password is required");
-//       return false;
-//     }
+    if ($("#pass").val() == '') {
+      showError("Password is required");
+      return false;
+    }
 
-//     sendAjax($("#loginForm").attr("action"), $("#loginForm").serialize());
+    sendAjax($("#loginForm").attr("action"), $("#loginForm").serialize());
 
-//     return false;
-//   }
+    return false;
+  }
 
-//   const LoginForm = (props) => {
-//     return (
-//       <form id="loginForm" name="loginForm" onSubmit={handleLogin} action="/login" method="POST" className="mainForm">
-//         <div className="greeting">
-//           <img className="icon" src="./assets/img/largeLogo.png" alt="logo for Shmood" />
-//           <h1>WELCOME</h1>
-//         </div>
+  const LoginForm = (props) => {
+    return (
+      <form id="loginForm" name="loginForm" onSubmit={handleLogin} action="/login" method="POST" className="mainForm">
+        <div className="greeting">
+          <img className="icon" src="./assets/img/largeLogo.png" alt="logo for Shmood" />
+          <h1>WELCOME</h1>
+        </div>
 
-//         <div className="fields">
-//           <div className="form-group col-xs-4">
-//             <input className="field" id="user" type="text" name="username" placeholder="Username" />
-//           </div>
-//           <div className="form-group col-xs-4">
-//             <input className="field" id="pass" type="password" name="pass" placeholder="Password" />
-//           </div>
-//         </div>
-//         <input id="loginCsrf" type="hidden" name="_csrf" value={props.csrf} />
-//         <button className="btn btn-secondary rounded-pill" id="loginButton" type="submit">Sign In</button>
+        <div className="fields">
+          <div className="form-group col-xs-4">
+            <input className="field" id="user" type="text" name="username" placeholder="Username" />
+          </div>
+          <div className="form-group col-xs-4">
+            <input className="field" id="pass" type="password" name="pass" placeholder="Password" />
+          </div>
+        </div>
+        <input id="loginCsrf" type="hidden" name="_csrf" value={props.csrf} />
+        <button className="btn btn-secondary rounded-pill" id="loginButton" type="submit">Sign In</button>
         
-//         <div className="alert alert-danger error" role="alert">
-//           Username or password is incorrect
-//         </div>
-//         <p>Don't have an account? <a href="./signup">Sign Up</a></p>
-//     </form>
-//     );
-//   };
+        <div className="alert alert-danger error" role="alert">
+          Username or password is incorrect
+        </div>
+        <p>Don't have an account? <a href="./signup">Sign Up</a></p>
+    </form>
+    );
+  };
 
-//   const setup = function(csrf){
-//     ReactDOM.render(
-//       <LoginForm csrf={csrf} />, document.querySelector('#login')
-//     );
-//   };
+  const setup = function(csrf){
+    ReactDOM.render(
+      <LoginForm csrf={csrf} />, document.querySelector('#login')
+    );
+  };
 
-//   const getToken = () => {
-//     sendGenericAjax('GET', '/getToken', null, (result) => {
-//         setup(result.csrfToken);
-//     });
-// };
+  const getToken = () => {
+    sendGenericAjax('GET', '/getToken', null, (result) => {
+        setup(result.csrfToken);
+    });
+};
